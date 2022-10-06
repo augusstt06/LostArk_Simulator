31f55951261d4e80c9b298828889de8e4bc699d3
augusstt06 <augusstt06@gmail.com>
2022-10-06 19:07:27 +0900

    BackEnd : Create Tripod Crawling Module

--- backend/module/crawling/tripod.js
+++ backend/module/crawling/tripod.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
@@ -12,30 +11,45 @@ exports.getTripod = async (url, id) => {
       const page = await browser.newPage();
 
       await page.goto(encodeURI(url + `${id}`));
+      const evaluate_Variables = process.env.CRAW_SKILL_CLICK;
 
-      await page.evaluate((i) => {
-        document
-          .querySelector(
-            `#profile-skill > div.profile-skill-battle > div.profile-skill__list > div:nth-child(${i}) > a`
-          )
-          .click();
-      }, i);
+      await page.evaluate(
+        (i, evaluate_Variables) => {
+          document.querySelector(`${evaluate_Variables}(${i}) > a`).click();
+        },
+        i,
+        evaluate_Variables
+      );
 
       let content = await page.content();
-
       const $ = cheerio.load(content, { decodeEntities: true });
 
-      const test = $(
-        `#profile-skill > div.profile-skill-battle > div.profile-skill-tripod > h5`
-      ).text();
-      console.log(test);
-      browser.close();
+      const skillName = $(`${process.env.CRAW_SKILL_NAME}`).text();
+
+      tripod[skillName] = {};
+
+      for (j = 1; j <= 3; j++) {
+        const skillTripod_Name = $(
+          `${process.env.CRAW_SKILL_TRIPOD_PREV}(${j + 1}) > ${
+            process.env.CRAW_SKILL_TRIPOD_AFTER
+          } > span > font`
+        ).text();
+
+        const skillTripod_Level = $(
+          `${process.env.CRAW_SKILL_TRIPOD_PREV}(${j + 1}) > ${
+            process.env.CRAW_SKILL_TRIPOD_AFTER
+          } > span > div`
+        ).text();
+        if (skillTripod_Name.length !== 0) {
+          tripod[skillName][skillTripod_Name] = skillTripod_Level;
+        }
+      }
     };
     for (i = 1; i <= 8; i++) {
       await clickSkill(i);
     }
-
-    return;
+    console.log("Success Crawling Tripod!");
+    return tripod;
   } catch (err) {
     console.log(err);
   }
