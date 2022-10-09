611ae4cd887ac2c2676926809bb10be40d234116
augusstt06 <augusstt06@gmail.com>
2022-10-10 03:12:52 +0900

    BackEnd : Create Class Module

---
+++ backend/staticData/class/warrior/berserker/index.js
@@ -0,0 +1,15 @@
+class Berserker {
+  constructor(data) {
+    this.basicStat = data.basicStat;
+    this.battleStat = data.battleStat;
+    this.card = data.card;
+    this.engrave = data.engrave;
+    this.jewel = data.jewel;
+    this.tripod = data.tripod;
+  }
+  calaulateDmg() {
+    return;
+  }
+}
+
+module.exports.Berserker;
