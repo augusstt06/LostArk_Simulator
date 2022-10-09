bd1dba1e0644ae88a25a4cb5616fb3d0542465e1
augusstt06 <augusstt06@gmail.com>
2022-10-09 19:49:25 +0900

    BackEnd : Modify Tripod Cralwling Module

--- backend/module/crawling/tripod.js
+++ backend/module/crawling/tripod.js
@@ -1,13 +1,21 @@
+const axios = require("axios");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
 require("dotenv").config();
 
-process.setMaxListeners(21);
-
 exports.getTripod = async (url, id) => {
   try {
     const tripod = {};
+
+    const res = await axios.get(encodeURI(url + `${id}`));
+    const $ = cheerio.load(res.data);
+    const skill_num = $(
+      `#profile-skill > div.profile-skill-battle > div.profile-skill__list > div`
+    ).length;
+
+    process.setMaxListeners(skill_num);
+
     const clickSkill = async (i) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
@@ -24,6 +32,7 @@ exports.getTripod = async (url, id) => {
       );
 
       let content = await page.content();
+
       const $ = cheerio.load(content, { decodeEntities: true });
 
       const skillName = $(`${process.env.CRAW_SKILL_NAME}`).text();
@@ -52,9 +61,11 @@ exports.getTripod = async (url, id) => {
         browser.close();
       }
     };
-    for (i = 1; i <= 21; i++) {
+
+    for (i = 1; i <= skill_num; i++) {
       await clickSkill(i);
     }
+
     console.log("Success Crawling Tripod!");
 
     return tripod;
