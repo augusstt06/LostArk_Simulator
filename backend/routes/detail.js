42c67eabe2d55c10138d76d9e695dcecbf37945d
augusstt06 <augusstt06@gmail.com>
2022-11-02 11:43:18 +0900

    Add Comments in detail.js

--- backend/routes/detail.js
+++ backend/routes/detail.js
@@ -4,8 +4,12 @@ var router = express.Router();
 require("dotenv").config();
 
 var crawlingData = require("../module/index");
-var calculate = require("../module/calculate/index");
+
+// 직업별 계산 모듈 호출
+// 계산모듈을 꼭 직업별로 만들어야 하는가? => 아덴 영향 생각하기
+
 var warrior = require("../classData/class/warrior/index");
+
 const url = process.env.CRAW_URL;
 
 // 클래스 구분하여 모듈 호출
@@ -15,6 +19,12 @@ const divideClass = (data) => {
       const berserker = warrior.getBerserker(data);
       console.log("버서커로 빠지기");
       return berserker;
+    case "디스트로이어":
+      const destroyer = warrior.getDestryoter(data);
+      return destroyer;
+    case "워로드":
+      const warlord = warrior.getWarlord(data);
+      return warlord;
     case "블레이드":
       console.log(data["Jewel"]);
       return;
