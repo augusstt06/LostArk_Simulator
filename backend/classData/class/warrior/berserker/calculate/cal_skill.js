e40d1a65ac653dd14e9fad32d00dc2d344d3f30a
augusstt06 <augusstt06@gmail.com>
2022-11-16 11:27:11 +0900

    BackEnd : Add Coefficient in skill tripod

--- backend/classData/class/warrior/berserker/calculate/cal_skill.js
+++ backend/classData/class/warrior/berserker/calculate/cal_skill.js
@@ -1,5 +1,6 @@
 var berserkerData = require("../skill.json");
 
+// 아덴계산도 같이
 exports.getBerserker_Tripod_Dmg = (data) => {
   const atk_stat = data["Basic_Stat"]["공격력"];
   const tripod = data["Tripod"];
@@ -15,14 +16,18 @@ exports.getBerserker_Tripod_Dmg = (data) => {
         case 1:
           const tier1 = berserkerData[skill]["tripod"]["tier1"][each_tripod];
           const tier1_Level = tripod[skill][each_tripod];
-          console.log(i);
+
           if (tier1 === null) {
             i++;
             continue;
           } else {
-            const test = Object.keys(tier1)[0];
+            const increase_category = Object.keys(tier1)[0];
             console.log("tier1 : ", tier1);
             console.log("트포 레벨 : ", tier1_Level);
+            console.log(tier1_Level.slice(3), "???");
+            const increase_coef =
+              tier1[increase_category][tier1_Level.slice(3) - 1];
+            console.log(increase_coef);
             i++;
             continue;
           }
@@ -30,35 +35,37 @@ exports.getBerserker_Tripod_Dmg = (data) => {
           console.log(each_tripod);
           const tier2 = berserkerData[skill]["tripod"]["tier2"][each_tripod];
           const tier2_Level = tripod[skill][each_tripod];
-          console.log(i);
+
           if (tier2 === null) {
             i++;
             continue;
           } else {
-            const test = Object.keys(tier2)[0];
+            const increase_category = Object.keys(tier2)[0];
             console.log("tier2 : ", tier2);
-            console.log(test);
             console.log("트포 레벨 : ", tier2_Level);
+            console.log(tier2_Level.slice(3), "???");
+
             i++;
             continue;
           }
 
         case 3:
           const tier3 = berserkerData[skill]["tripod"]["tier3"][each_tripod];
-
           const tier3_Level = tripod[skill][each_tripod];
-          console.log(i);
+
           if (tier3 === null) {
             i++;
             continue;
           } else {
-            const test = Object.keys(tier3)[0];
+            const increase_category = Object.keys(tier3)[0];
             console.log("tier3 : ", tier3);
-            console.log(test);
             console.log("트포 레벨 : ", tier3_Level);
             i++;
             continue;
           }
+        // 위의 switch문이 완료되면 스킬 하나의 트포당 데미지 추가 항목이 계산이 완료됨
+        // 따라서 switch문이 끝나는 시점에 각 항목을 종합하여 리턴
+        // 최종적인 계산은 어디서?
         default:
           continue;
       }
--- backend/classData/class/warrior/index.js
+++ backend/classData/class/warrior/index.js
@@ -1,9 +1,5 @@
-var berserkerData = require("./berserker/skill.json");
-var warlordData = require("./warlord/skill.json");
-
 var berserker_tripod = require("../warrior/berserker/calculate/cal_skill");
-// 아덴계산도 같이
-// 일단 작성하고 calculate로 옮기기.
+
 exports.getBerserker = (data) => {
   // > 데미지 = 공격력 _ 스킬계수 _ 피해증가 _ 추가피해 _ 치명타 피해
   berserker_tripod.getBerserker_Tripod_Dmg(data);
