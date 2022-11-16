11f40cd183c61f0bc39df093ba7d368fbec6c9d1
augusstt06 <augusstt06@gmail.com>
2022-11-16 10:55:10 +0900

    BackEnd : Connecting skill tripod to Static data

--- backend/classData/class/warrior/index.js
+++ backend/classData/class/warrior/index.js
@@ -18,25 +18,50 @@ exports.getBerserker = (data) => {
         case 1:
           const tier1 = berserkerData[skill]["tripod"]["tier1"][each_tripod];
           const tier1_Level = tripod[skill][each_tripod];
-
           console.log(i);
-          console.log("tier1 : ", tier1);
-          console.log("트포 레벨 : ", tier1_Level);
-
-          i++;
-          continue;
+          if (tier1 === null) {
+            i++;
+            continue;
+          } else {
+            const test = Object.keys(tier1)[0];
+            console.log("tier1 : ", tier1);
+            console.log("트포 레벨 : ", tier1_Level);
+            i++;
+            continue;
+          }
         case 2:
+          console.log(each_tripod);
           const tier2 = berserkerData[skill]["tripod"]["tier2"][each_tripod];
+          const tier2_Level = tripod[skill][each_tripod];
           console.log(i);
-          console.log("tier2", tier2);
-          i++;
-          continue;
+          if (tier2 === null) {
+            i++;
+            continue;
+          } else {
+            const test = Object.keys(tier2)[0];
+            console.log("tier2 : ", tier2);
+            console.log(test);
+            console.log("트포 레벨 : ", tier2_Level);
+            i++;
+            continue;
+          }
+
         case 3:
           const tier3 = berserkerData[skill]["tripod"]["tier3"][each_tripod];
+
+          const tier3_Level = tripod[skill][each_tripod];
           console.log(i);
-          console.log("tier3", tier3);
-          i++;
-          continue;
+          if (tier3 === null) {
+            i++;
+            continue;
+          } else {
+            const test = Object.keys(tier3)[0];
+            console.log("tier3 : ", tier3);
+            console.log(test);
+            console.log("트포 레벨 : ", tier3_Level);
+            i++;
+            continue;
+          }
         default:
           continue;
       }
