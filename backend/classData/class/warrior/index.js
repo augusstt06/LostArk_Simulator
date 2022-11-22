9d0a628d5c54d834a5c008f7b235a2bd6346cea0
augusstt06 <augusstt06@gmail.com>
2022-11-22 17:30:55 +0900

    BackEnd : Create Engrave Cacluate module

--- backend/classData/class/warrior/index.js
+++ backend/classData/class/warrior/index.js
@@ -1,12 +1,14 @@
-var berserker_tripod = require("../warrior/berserker/calculate/cal_skill");
-var berserker_jewel = require("../../common_cal/cal_jewel");
-var berserker_card = require("../../common_cal/cal_card");
+var berserker_tripod_cal = require("../warrior/berserker/calculate/cal_skill");
+var jewel_cal = require("../../common_cal/cal_jewel");
+var card_cal = require("../../common_cal/cal_card");
+var engrave_cal = require("../../common_cal/cal_engrave");
 
 exports.getBerserker = (data) => {
   // > 데미지 = 공격력 _ 스킬계수 _ 피해증가 _ 추가피해 _ 치명타 피해
-  berserker_tripod.getBerserker_Tripod_Increase(data["Tripod"]);
-  berserker_jewel.getJewel_Increase(data["Jewel"]);
-  berserker_card.getCard_Increase(data["Card"]);
+  berserker_tripod_cal.getBerserker_Tripod_Increase(data["Tripod"]);
+  jewel_cal.getJewel_Increase(data["Jewel"]);
+  card_cal.getCard_Increase(data["Card"]);
+  engrave_cal.getEngrave_Increase(data["Engrave"]);
 };
 exports.getDestroyer = (data) => {
   return;
---
+++ backend/classData/common_cal/cal_engrave.js
@@ -0,0 +1,4 @@
+exports.getEngrave_Increase = (data) => {
+  console.log(data);
+  return;
+};
