1fa9daed43f157bde5de4d04d9c6f9a43cb38d2a
augusstt06 <augusstt06@gmail.com>
2022-11-22 17:34:07 +0900

    BackEnd : Create Engrave Cacluate module

--- backend/classData/common_cal/cal_engrave.js
+++ backend/classData/common_cal/cal_engrave.js
@@ -1,4 +1,10 @@
 exports.getEngrave_Increase = (data) => {
   console.log(data);
+  let engrave_coef = {
+    increase_dmg: 0,
+    add_dmg: 0,
+    critical_dmg: 0,
+    increase_atk: 0,
+  };
   return;
 };
