79cbe6adc03dea23f62e2fd084f1f23f077f031b
augusstt06 <augusstt06@gmail.com>
2022-11-23 16:11:21 +0900

    BackEnd : Add User Engrave Calulator Module

--- backend/classData/class/warrior/index.js
+++ backend/classData/class/warrior/index.js
@@ -8,7 +8,7 @@ exports.getBerserker = (data) => {
   berserker_tripod_cal.getBerserker_Tripod_Increase(data["Tripod"]);
   jewel_cal.getJewel_Increase(data["Jewel"]);
   card_cal.getCard_Increase(data["Card"]);
-  engrave_cal.getEngrave_Increase(data["Engrave"]);
+  engrave_cal.getEngrave_Increase(data["Engrave"], data["Class"]);
 };
 exports.getDestroyer = (data) => {
   return;
--- backend/classData/common_cal/cal_engrave.js
+++ backend/classData/common_cal/cal_engrave.js
@@ -1,10 +1,42 @@
-exports.getEngrave_Increase = (data) => {
-  console.log(data);
+var pro_engraveData = require("../pro_engrave.json");
+var public_engraveData = require("../public_engrave.json");
+
+// 직업 필요
+exports.getEngrave_Increase = (data_engrave, data_class) => {
+  console.log(data_engrave);
   let engrave_coef = {
     increase_dmg: 0,
     add_dmg: 0,
     critical_dmg: 0,
     increase_atk: 0,
   };
-  return;
+  const user_engrave = Object.keys(data_engrave);
+
+  for (i in user_engrave) {
+    if (user_engrave[i] in pro_engraveData[data_class]) {
+      const increase_category = Object.keys(
+        pro_engraveData[data_class][user_engrave[i]]
+      );
+      const pro_engrave_coef =
+        pro_engraveData[data_class][user_engrave[i]][increase_category][
+          Number(data_engrave[user_engrave[i]]) - 1
+        ];
+
+      engrave_coef[increase_category] += pro_engrave_coef;
+    } else if (user_engrave[i] in public_engraveData) {
+      const increase_category = Object.keys(
+        public_engraveData[user_engrave[i]]
+      );
+      const public_engrave_coef =
+        public_engraveData[user_engrave[i]][increase_category][
+          Number(data_engrave[user_engrave[i]]) - 1
+        ];
+
+      engrave_coef[increase_category] += public_engrave_coef;
+    } else {
+      continue;
+    }
+  }
+  console.log("Engrave Coef : ", engrave_coef);
+  return engrave_coef;
 };
