da083a2e590d9ec4f6e7fd4f781ee306b8fadad4
augusstt06 <augusstt06@gmail.com>
2022-12-07 22:05:55 +0900

    Frontend : Using Response Data for  Rendering browser

--- dukii/src/views/DetailView.vue
+++ dukii/src/views/DetailView.vue
@@ -3,7 +3,30 @@
     <div class="head">
       <HeaderComponent />
       <InputComponent />
-      <!-- <button @click="ConnectTest">Connecting Test</button> -->
+    </div>
+    <div class="body">
+      <div class="text-center" v-if="Object.keys(userExpedition).length < 1">
+        <b-spinner
+          style="width: 3rem; height: 3rem"
+          variant="primary"
+          label="Spinning"
+        ></b-spinner>
+      </div>
+
+      <div v-if="Object.keys(userExpedition).length >= 1">
+        <b-button v-b-toggle.collapse-1 variant="primary">원정대</b-button>
+        <b-collapse id="collapse-1" class="mt-2">
+          <b-card>
+            <p class="card-text">{{ userExpedition }}</p>
+            <b-button v-b-toggle.collapse-1-inner size="sm"
+              >Toggle Inner Collapse</b-button
+            >
+            <b-collapse id="collapse-1-inner" class="mt-2">
+              <b-card>Hello!</b-card>
+            </b-collapse>
+          </b-card>
+        </b-collapse>
+      </div>
     </div>
   </div>
 </template>
@@ -24,16 +47,16 @@ export default defineComponent({
   data() {
     return {
       userId: this.$route.query.id,
+      variant: "info",
+      userExpedition: [],
+      userCard: [],
+      userEngrave: [],
+      userJewel: [],
+      userLevel: [],
+      userSkill: [],
     };
   },
-  methods: {
-    // async ConnectTest() {
-    //   await axios
-    //     .get(`/api/detail?id=${this.userId}`)
-    //     .then((res) => console.log(res))
-    //     .catch((err) => console.log(err));
-    // },
-  },
+  methods: {},
   watch: {
     $route(newValue, oldValue) {
       console.log(newValue.query.id, oldValue.query.id);
@@ -43,7 +66,15 @@ export default defineComponent({
   created() {
     axios
       .get(`/api/detail?id=${this.userId}`)
-      .then((res) => console.log(res))
+      .then((res) => {
+        this.userExpedition = res.data["Expedition"];
+        console.log(this.userExpedition);
+        this.userCard = res.data["Card"];
+        this.userEngrave = res.data["Engrave"];
+        this.userJewel = res.data["Jewel"];
+        this.userLevel = res.data["Level"];
+        this.userSkill = res.data["Tripod"];
+      })
       .catch((err) => console.log(err));
   },
 });
@@ -55,8 +86,15 @@ export default defineComponent({
   justify-content: center;
   align-content: center;
   text-align: center;
-  height: 90%;
+  height: 100%;
   width: 100%;
   padding: 0 50px;
+  /* background: blue; */
+}
+.head {
+  /* background: violet; */
+  max-width: 70%;
+  min-width: 50%;
+  margin: 40px auto;
 }
 </style>
