86b02b9696b5cc4c5d111f91b3248db9ef07441c
augusstt06 <augusstt06@gmail.com>
2022-12-07 20:57:39 +0900

    BackEnd : Modify skill.js

--- backend/classData/class/warrior/index.js
+++ backend/classData/class/warrior/index.js
@@ -5,9 +5,23 @@ var engrave_cal = require("../../common_cal/cal_engrave");
 
 exports.getBerserker = (data) => {
   // > 데미지 = 공격력 _ 스킬계수 _ 피해증가 _ 추가피해 _ 치명타 피해
+  // 스킬을 변수선언 시켜놓고 각각 dic으로 만들어 놓는다
+  const user_skill = Object.keys(data["Tripod"]);
+  const skill_coef = {};
+
+  for (let i in user_skill) {
+    skill_coef[user_skill[i]] = {};
+  }
+
+  console.log(skill_coef);
   const b_tripod = berserker_tripod_cal.getBerserker_Tripod_Increase(
     data["Tripod"]
   );
+  const tripod_skill = Object.keys(b_tripod);
+  console.log(tripod_skill, "11");
+
+  console.log(skill_coef);
+
   const b_jewel = jewel_cal.getJewel_Increase(data["Jewel"]);
   const b_card = card_cal.getCard_Increase(data["Card"]);
   const b_engrave = engrave_cal.getEngrave_Increase(
